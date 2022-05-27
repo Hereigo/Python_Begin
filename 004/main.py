0d9b247f52a994cd7fa9fc43df330dde29288135
hereigo <hereigo@online.ua>
2022-05-27 21:10:51 +0300

    Day 5

--- 004/main.py
+++ 004/main.py
@@ -1,11 +1,11 @@
-print(' =========== LISTS : =========== ')
+# =========== LISTS : ===========
 list = ['aaa', 111, 22.33, True, None]
 
 # SLICE - [FROM : TILL : STEP] :
 backward = list[::-1]
 print(backward)  # [None, True, 22.33, 111, 'aaa']
 
-print(' =========== LIST of FUNCs : =========== ')
+# =========== LIST of FUNCs : ===========
 f_list = [print, len]
 a_list = ['aaa', 'ssss', 'dddddd']
 
@@ -19,3 +19,11 @@ for f in f_list:
 # NO OUT-OF-RANGE-EXCEPTION! :
 print(a_list[2:10])  # 'dddddd'
 
+# CLONE list by SLICE :
+new_list = a_list[:]  # from first to last
+
+string1 = 'qazxswedcvfr'
+string2 = string1[:]
+
+print(new_list is a_list)  # False!
+print(string1 is string2)  # True !
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Python Begin.
+
+
+#### Remember default monitor on boot:
+```sh
+sudo cp ~/.config/monitors.xml ~gdm/.config/
+```
